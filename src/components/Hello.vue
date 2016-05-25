0e9ce4af4e8f6a660c7f2b68556c25c7f1cbfe43
shumwayjs <alf.shumway7@gmail.com>
2016-05-25 08:43:10 -0400

    first

---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
+
+<template>
+<div class="hello">
+    <h1>{{ msg }}</h1>
+</div>
+</template>
+
+<script>
+import jquery from 'jquery';
+
+export default {
+        data() {
+            return {
+                // note: changing this line won't causes changes
+                // with hot-reload because the reloaded component
+                // preserves its current state and we are modifying
+                // its initial state.
+                msg: 'Hello World!'
+            }
+        }
+}
+</script>
---
+++ src/components/employees.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+
+<div>
+    <select v-model="selectedUser">
+        <option v-for="user in users" v-bind:value="user">
+            {{ user.firstName + ' ' + user.lastName }}
+        </option>
+    </select>
+    <span v-if="selectedUser">Selected: {{ selectedUser.firstName }}</span>
+</div>
+
+</template>
+
+<script>
+
+import jquery from 'jquery';
+export default {
+    init() {
+            jquery.ajax({
+                url: '/api/users',
+                method: 'GET'
+            }).then(users => {
+                this.$data.users = users;
+            });
+        },
+        data() {
+            return {
+                users: undefined,
+                selectedUser: undefined
+            }
+        }
+}
+
+</script>
